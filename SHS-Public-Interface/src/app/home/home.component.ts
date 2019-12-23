4ec0b04c20f35b76fc183e803b4ac07c617672a7
ubaidurrehman87 <ubaidurrehman225786@gmail.com>
2019-12-23 10:04:13 +0500

    all Project

--- SHS-Public-Interface/src/app/home/home.component.ts
+++ SHS-Public-Interface/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,25 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  AddtoCart(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You want to Buy!",
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Add it!'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Added!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
   }
 
 }
